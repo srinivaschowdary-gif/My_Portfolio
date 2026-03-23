2257b44ac5cde043ea72280f02cee046a6043640
Kancharla Srinivas Chowdary <kancharlasrinu11@gmail.com>
2026-03-23 18:01:55 +0530

    Add typing animation and particles.js integration

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+new Typed("#typing",{
+strings:["Software Developer","Java Programmer","Web Developer"],
+typeSpeed:100,
+backSpeed:50,
+loop:true
+});
 
+particlesJS.load('particles-js','particles.json');
