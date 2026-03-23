bdb5d10d4c9de5504f2f24d93a2a57771f0aab30
Kancharla Srinivas Chowdary <kancharlasrinu11@gmail.com>
2026-03-23 18:47:24 +0530

    Revise typing animation and clean up script.js
    Updated typing animation strings and removed unused code.

--- script.js
+++ script.js
@@ -1,65 +1,13 @@
-/* typing animation */
+var typed = new Typed("#typing",{
 
-new Typed("#typing",{
-
-strings:["Software Developer","Java Programmer","Web Developer"],
+strings:[
+"Software Developer",
+"Java Programmer",
+"Problem Solver"
+],
 
 typeSpeed:100,
-
 backSpeed:50,
-
 loop:true
 
-})
-
-/* GitHub API projects */
-
-fetch("https://api.github.com/users/yourusername/repos")
-
-.then(res=>res.json())
-
-.then(data=>{
-
-let container=document.getElementById("project-container")
-
-data.slice(0,6).forEach(repo=>{
-
-container.innerHTML+=`
-
-<div class="project-card">
-
-<h3>${repo.name}</h3>
-
-<p>${repo.description || "No description"}</p>
-
-<a href="${repo.html_url}" target="_blank">View Code</a>
-
-</div>
-
-`
-
-})
-
-})
-
-/* scroll animation */
-
-ScrollReveal().reveal('.card',{
-
-delay:200,
-
-distance:'50px',
-
-origin:'bottom'
-
-})
-
-ScrollReveal().reveal('.project-card',{
-
-delay:200,
-
-distance:'50px',
-
-origin:'bottom'
-
-})
+});
